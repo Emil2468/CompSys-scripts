d1f9ca5f9594ae46279eeed3630a36edf95ebd0c
Ulrik Larsen <kazzak1998@gmail.com>
2019-01-04 12:13:24 +0100

    Fixed VPO calculations and var name

--- VMAddressTranslation.js
+++ VMAddressTranslation.js
@@ -2,15 +2,15 @@
 //virtual address to be used
 var vAddress = 0x03d4; //Change this to fit exercise
 var pageSize = 64; //Change this to fit exercise
-var tblSetSize = 4; //Change this to fit exercise
+var tblNrOfSets = 4; //Change this to fit exercise
 //number of bits in virtual address
 var vAddressLen = 14; //Change this to fit exercise
 
 
-function translateAddress(vAddress, pageSize, tblSetSize, vAddressLen) {
+function translateAddress(vAddress, pageSize, tblNrOfSets, vAddressLen) {
     var p = Math.log2(pageSize);
-
-    var t = Math.log2(tblSetSize);
+    
+    var t = Math.log2(tblNrOfSets);
 
     var bits = toBin(vAddress, vAddressLen);
     console.log("Bits in virtual address: " + bits);
@@ -18,19 +18,19 @@ function translateAddress(vAddress, pageSize, tblSetSize, vAddressLen) {
     //Shift in order to only keep the bits needed to vpn
     var vpn = vAddress >>> p;
     console.log("VPN (HEX): " + vpn.toString(16));
-    console.log("VPN (BIN): " + vpn.toString(2));
+    console.log("VPN (BIN): " + toBin(vpn, vAddressLen - p));
 
-    var tlbI = vpn & bitMask(t);
+    var tlbI = (vAddress >>> p) & bitMask(t);
     console.log("TBL Index (HEX): " + tlbI.toString(16));
-    console.log("TBL Index (BIN): " + tlbI.toString(2));
+    console.log("TBL Index (BIN): " + toBin(tlbI, t));
 
-    var tlbT = vpn >>> t;
+    var tlbT = vAddress >>> (p + t);
     console.log("TBL Tag (HEX): " + tlbT.toString(16));
-    console.log("TBL Tag (BIN): " + tlbT.toString(2));
+    console.log("TBL Tag (BIN): " + toBin(tlbT, p + t));
 
-    var vpo = vAddress & bitMask(p - 1);
+    var vpo = vAddress & bitMask(p);
     console.log("VPO (HEX): " + vpo.toString(16));
-    console.log("VPO (BIN): " + vpo.toString(2));
+    console.log("VPO (BIN): " + toBin(vpo, p));
 
 }
 
