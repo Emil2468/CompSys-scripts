c4859f100c0d698d65c222c9d203bac459e51dfd
Emil2468 <emh@calaha.dk>
2019-01-09 10:56:24 +0100

    Sloppy hash, plus internet checksum

---
+++ internetCheckSum.js
@@ -0,0 +1,37 @@
+//Splits string into 16-bit integers then sums it up and prints 1s compliment of that, meaning the negated version
+//If string does not have even length, the last byte is 0
+function checkSumString(str) {
+    var sum = 0;
+    for(var i = 0; i < str.length; i+=2) {
+        var n1 = str.charCodeAt(i);
+        var n2 = str.charCodeAt(i+1);
+        var n = (n1 << 8) + n2;
+        console.log(str[i] + str[i+1] + ":\t\t " + toBin(n,16));
+        
+        sum += n;
+    
+    }
+    console.log("Sum:\t\t " + toBin(~sum % 65536, 16)); 
+    console.log("1s compliment:\t " + toBin(~sum % 65536, 16));  
+}
+
+checkSumString("CHKSUM");
+
+
+//Converts number to string of len bits, I use this to get correct amount of bits
+function toBin(num, len) {
+    var res = "";
+    var temp = 0;
+    for (var i = len - 1; i >= 0; i--) {
+        temp = num & parseInt(Math.pow(2, i));
+        if (temp == 0) {
+            res += "0";
+        } else {
+            res += "1";
+        }
+        if (i % 4 == 0) {
+            res += " ";
+        }
+    }
+    return res;
+}
---
+++ sloppyHashPage640.js
@@ -0,0 +1,45 @@
+//Couldn't find a good description of this, so I assume two things:
+//1) the message is padded with zeros if length is not divisible by 4
+//2) That there won't be exercises with this in the exam since we haven't used it much in the course
+
+//checkSumInt(65536 * 65536 - 1);
+checkSumString('CHKSUM');
+function checkSumString(str) {
+    var sums = [];
+    for (var i = 0; i < 4; i++) {
+        sums[i] = 0;
+    }
+    var res = "";
+    for (var i = 0; i < str.length; i++) {
+        if (i % 4 == 0) {
+            console.log(res);
+            res = "";
+        }
+        res += str.charCodeAt(i).toString(16) + "   ";
+
+        sums[i % 4] = (sums[i % 4] + str.charCodeAt(i)) % 0xff;
+    }
+    //Since the last line won't be printed in the loop, print it here
+    console.log(res);
+    console.log("__________________")
+    res = "";
+    for (var i = 0; i < 4; i++) {
+        res += sums[i].toString(16) + "   ";
+
+    }
+    console.log(res);
+
+}
+
+
+//Returns the n'th byte from num, 0th being the least significant byte
+function extractByte(n, num) {
+    var mask = 0;
+    for (var i = n * 8; i < (n + 1) * 8; i++) {
+        //Add 8 bits set to one in correct position
+        mask += parseInt(Math.pow(2, i));
+    }
+    //bitwise and to set all other bytes to 0
+    //then shift so we only have one relevant byte
+    return (mask & num) >> (n * 8);
+}
