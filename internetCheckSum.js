98a42484bd5bc675dbf90acc5c2fade1ee0890a4
Emil2468 <emh@calaha.dk>
2019-01-09 11:08:26 +0100

    Fixed bug and added comments

--- internetCheckSum.js
+++ internetCheckSum.js
@@ -1,13 +1,20 @@
+//There is no wrap around in case of overflow, as I can't tell for sure 
+//if this is a thing here, or if it is only in UDP-checksum
+
+
 //Splits string into 16-bit integers then sums it up and prints 1s compliment of that, meaning the negated version
 //If string does not have even length, the last byte is 0
 function checkSumString(str) {
     var sum = 0;
     for(var i = 0; i < str.length; i+=2) {
         var n1 = str.charCodeAt(i);
+
+        //Set n2 to zero in case string is not any longer
         var n2 = 0;
         if(i + 1 < str.length) {
             n2 = str.charCodeAt(i+1);
         }
+        //Shift n1 8 bits since this is most significant byte
         var n = (n1 << 8) + n2;
         if(i + 1 < str.length) {
             console.log(str[i] + str[i+1] + ":\t\t\t" + toBin(n,16));
@@ -15,15 +22,15 @@ function checkSumString(str) {
             console.log(str[i] + "NO_CHAR:\t\t" + toBin(n,16));
         }
         
-        
         sum += n;
     
     }
-    console.log("Sum:\t\t\t" + toBin(~sum % 65536, 16)); 
+    //Use modulo 2^16 to make sure number is no longer than 16 bits
+    console.log("Sum:\t\t\t" + toBin(sum % 65536, 16)); 
     console.log("1s compliment:\t\t" + toBin(~sum % 65536, 16));  
 }
 
-checkSumString("CHKSUM!");
+checkSumString("CHKSUM!!");
 
 
 //Converts number to string of len bits, I use this to get correct amount of bits
