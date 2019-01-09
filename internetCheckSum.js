e972a108e72da801b02aa9f6a609ac7f8d1c6c40
Emil2468 <emh@calaha.dk>
2019-01-09 10:59:03 +0100

    Can now internet checksum odd lengths

--- internetCheckSum.js
+++ internetCheckSum.js
@@ -4,18 +4,26 @@ function checkSumString(str) {
     var sum = 0;
     for(var i = 0; i < str.length; i+=2) {
         var n1 = str.charCodeAt(i);
-        var n2 = str.charCodeAt(i+1);
+        var n2 = 0;
+        if(i + 1 < str.length) {
+            n2 = str.charCodeAt(i+1);
+        }
         var n = (n1 << 8) + n2;
-        console.log(str[i] + str[i+1] + ":\t\t " + toBin(n,16));
+        if(i + 1 < str.length) {
+            console.log(str[i] + str[i+1] + ":\t\t\t" + toBin(n,16));
+        } else {
+            console.log(str[i] + "NO_CHAR:\t\t" + toBin(n,16));
+        }
+        
         
         sum += n;
     
     }
-    console.log("Sum:\t\t " + toBin(~sum % 65536, 16)); 
-    console.log("1s compliment:\t " + toBin(~sum % 65536, 16));  
+    console.log("Sum:\t\t\t" + toBin(~sum % 65536, 16)); 
+    console.log("1s compliment:\t\t" + toBin(~sum % 65536, 16));  
 }
 
-checkSumString("CHKSUM");
+checkSumString("CHKSUM!");
 
 
 //Converts number to string of len bits, I use this to get correct amount of bits
